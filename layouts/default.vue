77b34f396870e00e9adcc0df40a57bc5ddfa2ff2
Steven Cabugos <stevencabugos138@gmail.com>
2025-04-21 16:17:34 +0800

    feat: add seo

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,3 +9,16 @@
   </div>
 </template>
 
+<script setup>
+useSeoMeta({
+  description: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
+  ogTitle: 'RV Rioflorido Construction',
+  ogDescription: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
+  ogImage: '/images/logo/thumbnail-logo.png',
+  ogUrl: 'https://rioflorido3213.netlify.app/',
+  twitterTitle: 'RV Rioflorido Construction',
+  twitterDescription: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
+  twitterImage: '/images/logo/thumbnail-logo.png',
+  twitterCard: 'summary'
+})
+</script>
