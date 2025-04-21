bfcf0a82b0ef966ce1e9399b384df78151e8020e
Steven Cabugos <stevencabugos138@gmail.com>
2025-04-21 16:41:38 +0800

    test: chore of thumbnail logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,15 +11,16 @@
 
 <script setup>
 useSeoMeta({
+  title: 'RV Rioflorido Construction',
   description: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
   ogTitle: 'RV Rioflorido Construction',
   ogDescription: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
-  ogImage: '/images/logo/thumbnail-logo.png',
-  ogUrl: 'https://rioflorido3213.netlify.app/',
+  ogImage: '/thumbnail-logo.png',
+  ogUrl: '[og:url]',
   twitterTitle: 'RV Rioflorido Construction',
   twitterDescription: 'Your Dream Home Starts Here, Experienced Civil Engineers Delivering Complete Construction Solutions',
-  twitterImage: '/images/logo/thumbnail-logo.png',
-  twitterCard: 'summary'
+  twitterImage: '/thumbnail-logo.png',
+  twitterCard: 'summary_large_image',
 })
 
 useHead({
