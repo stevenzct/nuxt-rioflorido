7d70902aeb7180378643cb837272ed182f366a1e
Steven Cabugos <stevencabugos138@gmail.com>
2025-05-19 09:06:35 +0800

    feat: remove @nuxthub/core

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxthub/core'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/image'],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   components: true,
