5a48dd780df77ea905553f2b5d3ef0d88614b504
Steven Cabugos <stevencabugos138@gmail.com>
2025-05-19 14:17:13 +0800

    feat: add icon image for google search results

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,8 +40,8 @@ export default defineNuxtConfig({
         { name: "robots", content: "index, follow" },
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/public/images/logo/secondary-logo.ico'     
-        },
+        { rel: 'icon', type: 'image/x-icon', href: '/images/logo/secondary-logo.ico'},
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/images/logo/secondary-logo.ico" },
         {
           rel: "preload",
           as: "style",
