7d4c64c1bb4917c4b6bc35299ad7a45541b97332
Steven Cabugos <stevencabugos138@gmail.com>
2025-06-03 17:49:36 +0800

    feat: remove vue lazy

--- plugins/vue-lazyload.js
+++
@@ -1,9 +0,0 @@
-// // plugins/vue-lazyload.js
-// // import VueLazyload from 'vue-lazyload';
-
-// export default defineNuxtPlugin(nuxtApp => {
-//   nuxtApp.vueApp.use(VueLazyload, {
-//     loading: '/images/loading.gif',  // Optional: specify a loading placeholder image
-//     attempt: 1,  // Number of attempts to load an image
-//   });
-// });
