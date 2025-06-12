184237e2f3aacda9e0e9388bf5db197be3e2828e
stevenzct <stevencabugos138@gmail.com>
2025-06-12 15:55:19 +0800

    feat: change headline 3xl to 4xl font size

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
         <div class="w-full">
           <div class="max-w-screen-2xl mx-auto px-4 py-8">
             <h1
-              class="text-white text-3xl md:text-7xl lg:text-8xl  font-neue-montreal font-bold"
+              class="text-white text-4xl md:text-7xl lg:text-8xl  font-neue-montreal font-bold"
             >
               Design, Build, Construct
             </h1>
